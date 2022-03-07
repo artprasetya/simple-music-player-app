c4c67f30cdfe415afbb3e89e559b09f7b4e31e1a
artprasetya <artiyan.prasetya@payfazz.com>
2022-03-07 08:07:13 +0700

    helper: add generate icon helper

--- lib/helper/helper.dart
+++ lib/helper/helper.dart
@@ -1,7 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
 class Helper {
   // method to replace space with plus sign
   static String? replaceSpace(String? query) {
     if (query == null) return null;
     return query.trim().replaceAll(" ", "+");
+  }
+
+  // generate icon depend on state
+  static Widget generateIcon({
+    bool? isPlaying = false,
+    ProcessingState? processingState,
+    VoidCallback? onTap,
+  }) {
+    if (processingState == ProcessingState.loading ||
+        processingState == ProcessingState.buffering) {
+      return Container(
+        margin: EdgeInsets.all(8),
+        child: CircularProgressIndicator(),
+      );
+    } else if (isPlaying != true) {
+      return IconButton(
+        icon: Icon(Icons.play_arrow),
+        iconSize: 32,
+        onPressed: onTap,
+      );
+    } else if (processingState != ProcessingState.completed) {
+      return IconButton(
+        icon: Icon(Icons.pause),
+        iconSize: 32,
+        onPressed: onTap,
+      );
+    } else {
+      return IconButton(
+        icon: Icon(Icons.replay),
+        iconSize: 32,
+        onPressed: onTap,
+      );
+    }
   }
 }
