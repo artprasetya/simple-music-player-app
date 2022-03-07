e34e0998579223a62a93ec51da8fa1b8222b9a26
artprasetya <artiyan.prasetya@payfazz.com>
2022-03-07 08:10:17 +0700

    [MainPage] update: wrap page with BLoC

--- lib/modules/main/page.dart
+++ lib/modules/main/page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:simple_music_player_app/models/result.dart';
+import 'package:simple_music_player_app/modules/main/blocs/media_player_bloc/bloc.dart';
 import 'package:simple_music_player_app/modules/main/blocs/music_bloc/bloc.dart';
-import 'package:simple_music_player_app/modules/main/components/lists/music_list.dart';
+import 'package:simple_music_player_app/modules/main/components/media_player.dart';
+import 'package:simple_music_player_app/modules/main/components/music_list.dart';
 import 'package:simple_music_player_app/modules/main/initiator.dart';
 import 'package:simple_music_player_app/modules/main/view.dart';
 
@@ -29,20 +33,56 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder(
-      bloc: _initiator.musicListBloc,
-      builder: (context, MusicListState state) {
-        return MainView(
-          musicListProps: MusicListProps(
-            isLoading: state is MusicListLoading,
-            errorMessage: state is MusicListError ? state.errorMessage : null,
-            musicList: state is MusicListLoaded ? state.musics ?? [] : [],
-            onSearchChanged: _initiator.onSearchChanged,
-            searchController: _initiator.searchController,
-            searchFocusNode: _initiator.searchFocusNode,
-            onTapItem: _initiator.onTapItem,
-          ),
-        );
+    Result? _musicPlaying;
+    bool? _showMediaPlayer;
+
+    return BlocConsumer(
+      bloc: _initiator.playerBloc,
+      listener: (context, MediaPlayerState mpState) {
+        if (mpState is MediaPlayerOnListen) {
+          _musicPlaying = mpState.musicPlaying;
+          _showMediaPlayer = mpState.showPlayer;
+        }
+      },
+      builder: (context, MediaPlayerState mpState) {
+        PlayerState? _playerState;
+        bool? _isPlaying = false;
+        ProcessingState? _processingState;
+
+        return StreamBuilder<PlayerState>(
+            stream: _initiator.audioPlayer.playerStateStream,
+            builder: (context, snapshot) {
+              _playerState = snapshot.data;
+              _isPlaying = _playerState?.playing ?? false;
+              _processingState = _playerState?.processingState;
+
+              return BlocBuilder(
+                bloc: _initiator.bloc,
+                builder: (context, MusicListState state) {
+                  return MainView(
+                    musicListProps: MusicListProps(
+                      onPlayMusicTrackId: _musicPlaying?.trackId,
+                      isLoading: state is MusicListLoading,
+                      errorMessage:
+                          state is MusicListError ? state.errorMessage : null,
+                      musicList:
+                          state is MusicListLoaded ? state.musics ?? [] : [],
+                      onSearchChanged: _initiator.onSearchChanged,
+                      searchController: _initiator.searchController,
+                      searchFocusNode: _initiator.searchFocusNode,
+                      onTapItem: _initiator.onTapItem,
+                    ),
+                    showMediaPlayer: _showMediaPlayer,
+                    mediaPlayerProps: MediaPlayerProps(
+                      isPlaying: _isPlaying,
+                      processingState: _processingState,
+                      result: _musicPlaying,
+                      onTapPlayerButton: _initiator.onTapPlayerButton,
+                    ),
+                  );
+                },
+              );
+            });
       },
     );
   }
